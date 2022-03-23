76ede27a824c2f9a8513971c4b2a60cb408cdb1d
theappleboom <sethbeastalan@gmail.com>
2022-03-23 17:46:00 -0400

    Added list inspection and addition
    Goal lists can now be inspected on a basic level and added. The inspection needs to be fleshed out further, and the inspection page needs to be made its own widget for proper state control.

--- goal_list_training/lib/main.dart
+++ goal_list_training/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -40,13 +42,50 @@ class _MyHomePageState extends State<MyHomePage> {
     "Scratch on Tree",
   ];
   final _finished = <String>{};
-  final _goalList = <GoalListItem>{};
+  final _goalList = GoalList();
+
+  void _pushItemAdder() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => ItemAdder(goalList: _goalList)),
+    );
+  }
+
+  void _pushListEditor() {
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final tiles = _goalList.listItems.map((listItem) {
+        return ListTile(
+          title: Text(
+            listItem.name,
+            style: const TextStyle(fontSize: 20.0),
+          ),
+        );
+      });
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+          : <Widget>[];
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Current Goal List'),
+        ),
+        body: ListView(children: divided),
+        floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add), onPressed: _pushItemAdder),
+      );
+    }));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushListEditor,
+              tooltip: 'Inspect Goal List'),
+        ],
       ),
       body: Center(
         child: ListView.builder(
@@ -88,10 +127,100 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+class ItemAdder extends StatefulWidget {
+  const ItemAdder({Key? key, required this.goalList}) : super(key: key);
+  final GoalList goalList;
+
+  @override
+  State<ItemAdder> createState() => _ItemAdderState();
+}
+
+class _ItemAdderState extends State<ItemAdder> {
+  String _newItemName = "";
+  String _newItemRarity = "Common";
+
+  void _addNewItem() {
+    int rarity = 0;
+
+    if (_newItemName != "") {
+      switch (_newItemRarity) {
+        case "Common":
+          rarity = 0;
+          break;
+        case "Uncommon":
+          rarity = 1;
+          break;
+        case "Rare":
+          rarity = 2;
+          break;
+      }
+      widget.goalList.addListItem(_newItemName, rarity);
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a New Item'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: TextFormField(
+              onChanged: (newValue) {
+                setState(() {
+                  _newItemName = newValue.toString();
+                });
+              },
+              decoration: const InputDecoration(
+                labelText: 'New List Item',
+              ),
+            ),
+          ),
+          Container(
+            alignment: AlignmentDirectional.topStart,
+            padding: const EdgeInsets.all(24.0),
+            child: DropdownButton<String>(
+              hint: const Text('How often should this item show'),
+              value: _newItemRarity,
+              onChanged: (value) {
+                setState(() {
+                  _newItemRarity = value.toString();
+                });
+              },
+              items: <String>["Common", "Uncommon", "Rare"].map((itemValue) {
+                return DropdownMenuItem<String>(
+                  value: itemValue,
+                  child: Text(itemValue),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.check), onPressed: _addNewItem),
+    );
+  }
+}
+
 class GoalListItem {
   String name;
   int rarity;
   int weight;
 
   GoalListItem(this.name, this.rarity, this.weight);
+}
+
+class GoalList {
+  List<GoalListItem> listItems;
+
+  GoalList() : listItems = <GoalListItem>[];
+
+  addListItem(String name, int rarity) {
+    listItems.add(GoalListItem(name, rarity, 0));
+  }
 }
